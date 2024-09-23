6fcae2d91a20822f09c7131e80bc24190bbd3081
leetjes123 <lvdleetjes@gmail.com>
2024-09-23 15:24:46 +0200

    beter

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,24 +5,14 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 
+url = 'https://api.luchtmeetnet.nl/open_api/lki?page=1&order_by=timestamp_measured&order_direction=desc'
 
-luchtmeetnet_url = 'https://api.luchtmeetnet.nl/open_api/components/NO2'
-
-data = r.get(luchtmeetnet_url)
-
-<<<<<<< Updated upstream
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-
-
-HOI
-=======
+data = r.get(url)
 print(data.text)
+
 
 #st.title("🎈 My new app")
 #st.write(
 #    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 #)
->>>>>>> Stashed changes
+

ed7faf7ce3dc2e0e7266c8760a8a9281de51f574
leetjes123 <lvdleetjes@gmail.com>
2024-09-26 00:43:46 +0200

    Vanalles en nogwat veranderd.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,10 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 
-#COVID API
-#De covid API van api-ninjas.com geeft de COVID cijfers per dag aan.
-#De API is openbaar en gratis maar er is wel een Key vereist. Deze key heb ik in de code gezet zodat iedereen het kan runnen.
-#Het land van interesse kan worden aangepaste door de parameter ?country=Land aan te passen. Hier hebben we hem op Netherlands gezet.
-#De API response is een lijst van dictionaries, elke met een regio van het land. In dit geval betekent het aparte cijfers voor
-#de caribische gemeenten van het koninkrijk. Deze zijn zodanig klein dat ze verwaarloosbaar zijn.
+#########################
+# API COVID DATA ########
+#########################
+
 apiKey = 'BT2KC0xm+UHgWAr5kw889A==UlFfvOOZFfy9BkEp'
 baseUrl = 'https://api.api-ninjas.com/v1/covid19?country=Netherlands'
 #GET request
@@ -20,146 +18,109 @@ data = json.loads(response.text)[-1]
 #Laadt de data in een panda's dataframe.
 df_covid = pd.DataFrame(data) #DEZE VEREIST NOG AANPASSING
 
+########################################################
+# VERKEERSINTENSITEIT DATA LADEN EN FEATURES TOEVOEGEN #
+########################################################
+
+def load_and_cache_data():
+
+    df_2019 = pd.read_csv('intensiteit2019.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie'])
+    df_2020 = pd.read_csv('intensiteit2020.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie'])
+    df_2021 = pd.read_csv('intensiteit2021.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie'])
+    df_2022 = pd.read_csv('intensiteit2022.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie'])
+    df_2023 = pd.read_csv('intensiteit2023.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie'])
+    df_2024 = pd.read_csv('intensiteit2024.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie'])
+
+    df = pd.concat([df_2019,df_2020,df_2021,df_2022,df_2023,df_2024], ignore_index=True)
+
+    df['start_meetperiode'] = pd.to_datetime(df['start_meetperiode'])
+    df['jaar'] = df['start_meetperiode'].dt.year
+    df['dag'] = df['start_meetperiode'].dt.day_name()
+    df['tijd'] = df['start_meetperiode'].dt.strftime('%H:%M')
+    df['1W_bin'] = df['start_meetperiode'].dt.to_period('W').dt.start_time
+    
+
+    return df
+
+df = load_and_cache_data()
+
+###################################
+# INTRODUCTIE EN COVID PLOT #######
+###################################
 
 
+###################################
+# PLOTS VAN THOMAS MET UITLEG #####
+###################################
 
-#Verkeersintensititeit DataFrames inladen
-meetpuntenNaarStadsdeel = {
-    'RWS01_MONIBAS_0101hrl0033ra' : 'Noord',
-    'RWS01_MONIBAS_0101hrr0032ra' : 'Noord',
-    'RWS01_MONIBAS_0101hrl0129ra' : 'Oost',
-    'RWS01_MONIBAS_0101hrr0129ra' : 'Oost',
-    'RWS01_MONIBAS_0101hrl0175ra' : 'Zuid',
-    'RWS01_MONIBAS_0101hrr0175ra' : 'Zuid',
-    'RWS01_MONIBAS_0101hrl0239ra' : 'West',
-    'RWS01_MONIBAS_0101hrr0239ra' : 'West'
-    }
-#Ma 16-09-2024 - Zo 22-09-2024
+@st.cache_data
+def load_data(year):
+    df = pd.read_csv(f'intensiteit{year}_weekly.csv')
+    df['week_start'] = pd.to_datetime(df['week_start'])
+    return df
 
-df_2024 = pd.read_csv('intensiteit2024.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie', 'gem_snelheid'])
-df_2024['stadsdeel'] = df_2024['id_meetlocatie'].map(meetpuntenNaarStadsdeel)
-df_2024_grouped = df_2024.groupby(['start_meetperiode', 'stadsdeel']).agg({
-    'gem_intensiteit': 'sum',
-    'gem_snelheid': 'mean'
-}).reset_index()
+# Cache individual DataFrames
+df19 = load_data(2019)
+df20 = load_data(2020)
+df21 = load_data(2021)
+df22 = load_data(2022)
+df23 = load_data(2023)
+df24 = load_data(2024)
 
-df_2024['start_meetperiode'] = pd.to_datetime(df_2024['start_meetperiode'])
-df_2024['1W_bin'] = df_2024['start_meetperiode'].dt.to_period('W').dt.start_time
 
-fig_2024 = px.bar(df_2024, 
-             x='1W_bin', 
+dict = {
+    2019 : df19,
+    2020 : df20,
+    2021 : df21,
+    2022 : df22,
+    2023 : df23,
+    2024 : df24
+}
+
+st.write('Uitleg')
+year = st.selectbox("Selecteer een jaar", [2019,2020,2021,2022,2023,2024])
+# yearlyData = df[df['jaar'] == year]
+
+weekFig = px.bar(dict[year], 
+             x='week', 
              y='gem_intensiteit',
-             title="Intensiteit verkeerstromen in 2024 (per week)", 
-             labels={'1W_bin': 'Tijdstip (per week)', 'gem_intensiteit': 'Aantal'}, 
+             title=f"Intensiteit verkeerstromen in {year} (per week)", 
+             labels={'week': 'Tijdstip (per week)', 'gem_intensiteit': 'Aantal'}, 
              color_discrete_sequence=['coral'])
-#fig_2024.show()
 
-#df_2024_pivot = df_2024_grouped.pivot(index='start_meetperiode', columns='stadsdeel', 
-                           # values=['gem_intensiteit', 'gem_snelheid'])
+# Customize the layout
+weekFig.update_layout(xaxis_title='Tijdstip (per week)',
+                  yaxis_title='Aantal',
+                  hovermode='x unified')
 
-#Ma xx-09-2023 - Zo xx-09-2023
-df_2023 = pd.read_csv('intensiteit2023.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie', 'gem_snelheid'])
-df_2023['stadsdeel'] = df_2023['id_meetlocatie'].map(meetpuntenNaarStadsdeel)
-df_2023_grouped = df_2023.groupby(['start_meetperiode', 'stadsdeel']).agg({
-    'gem_intensiteit': 'sum',
-    'gem_snelheid': 'mean'
-}).reset_index()
+st.plotly_chart(weekFig, use_container_width=True)
 
-df_2023['start_meetperiode'] = pd.to_datetime(df_2024['start_meetperiode'])
-df_2023['1W_bin'] = df_2023['start_meetperiode'].dt.to_period('W').dt.start_time
+###################################
+# PLOT VAN WEEKDAG PER JAAR #######
+###################################
 
-fig_2023 = px.bar(df_2023, 
-             x='1W_bin', 
-             y='gem_intensiteit',
-             title="Intensiteit verkeerstromen in 2023 (per week)", 
-             labels={'1W_bin': 'Tijdstip (per week)', 'gem_intensiteit': 'Aantal'}, 
-             color_discrete_sequence=['coral'])
-#fig_2023.show()
+#Dagelijkse data laden
+@st.cache_data
+def calculate_daily_average(df):
+    return df.groupby(['dag', 'tijd','jaar'])['gem_intensiteit'].mean().reset_index()
 
-#Ma xx-09-2022 - Zo xx-09-2022
-df_2022 = pd.read_csv('intensiteit2022.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie', 'gem_snelheid'])
-df_2022['stadsdeel'] = df_2022['id_meetlocatie'].map(meetpuntenNaarStadsdeel)
-df_2024_grouped = df_2022.groupby(['start_meetperiode', 'stadsdeel']).agg({
-    'gem_intensiteit': 'sum',
-    'gem_snelheid': 'mean'
-}).reset_index()
+df_daily = calculate_daily_average(df)
 
-df_2022['start_meetperiode'] = pd.to_datetime(df_2022['start_meetperiode'])
-df_2022['1W_bin'] = df_2022['start_meetperiode'].dt.to_period('W').dt.start_time
+#Dropdown box maken en filteren op dag
+st.write('Uitleg')
+weekday = st.selectbox('Selecteer een dag', df_daily['dag'].unique())
+dailyData = df_daily[df_daily['dag'] == weekday]
 
-fig_2022 = px.bar(df_2022, 
-             x='1W_bin', 
-             y='gem_intensiteit',
-             title="Intensiteit verkeerstromen in 2022 (per week)", 
-             labels={'1W_bin': 'Tijdstip (per week)', 'gem_intensiteit': 'Aantal'}, 
-             color_discrete_sequence=['coral'])
-#fig_2022.show()
+#Plot
+dayFig = px.line(dailyData, x='tijd', y='gem_intensiteit', color='jaar',
+              title=f'Traffic Intensity on {weekday} - Comparison Across Years',
+              labels={'tijd': 'Time', 'gem_intensiteit': 'Average Intensity', 'year': 'Year'})
 
-#Ma xx-09-2021 - Zo xx-09-2021 
-df_2021 = pd.read_csv('intensiteit2024.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie', 'gem_snelheid'])
-df_2021['stadsdeel'] = df_2021['id_meetlocatie'].map(meetpuntenNaarStadsdeel)
-df_2021_grouped = df_2021.groupby(['start_meetperiode', 'stadsdeel']).agg({
-    'gem_intensiteit': 'sum',
-    'gem_snelheid': 'mean'
-}).reset_index()
+dayFig.update_layout(xaxis_title='Time of Day', yaxis_title='Average Intensity', hovermode='x')
 
-df_2021['start_meetperiode'] = pd.to_datetime(df_2021['start_meetperiode'])
-df_2021['1W_bin'] = df_2021['start_meetperiode'].dt.to_period('W').dt.start_time
+st.plotly_chart(dayFig, use_container_width=True)
 
-fig_2021 = px.bar(df_2021, 
-             x='1W_bin', 
-             y='gem_intensiteit',
-             title="Intensiteit verkeerstromen in 2021 (per week)", 
-             labels={'1W_bin': 'Tijdstip (per week)', 'gem_intensiteit': 'Aantal'}, 
-             color_discrete_sequence=['coral'])
-#fig_2021.show()
 
-#Ma xx-09-2020 - Zo xx-09-2020
-df_2020 = pd.read_csv('intensiteit2020.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie', 'gem_snelheid'])
-df_2020['stadsdeel'] = df_2020['id_meetlocatie'].map(meetpuntenNaarStadsdeel)
-df_2020_grouped = df_2020.groupby(['start_meetperiode', 'stadsdeel']).agg({
-    'gem_intensiteit': 'sum',
-    'gem_snelheid': 'mean'
-}).reset_index()
-
-df_2020['start_meetperiode'] = pd.to_datetime(df_2020['start_meetperiode'])
-df_2020['1W_bin'] = df_2020['start_meetperiode'].dt.to_period('W').dt.start_time
-
-fig_2020 = px.bar(df_2020, 
-             x='1W_bin', 
-             y='gem_intensiteit',
-             title="Intensiteit verkeerstromen in 2020 (per week)", 
-             labels={'1W_bin': 'Tijdstip (per week)', 'gem_intensiteit': 'Aantal'}, 
-             color_discrete_sequence=['coral'])
-#fig_2020.show()
-
-#Ma xx-09-2019 - Zo xx-09-2019
-df_2019 = pd.read_csv('intensiteit2019.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie', 'gem_snelheid'])
-df_2019['stadsdeel'] = df_2019['id_meetlocatie'].map(meetpuntenNaarStadsdeel)
-df_2019_grouped = df_2019.groupby(['start_meetperiode', 'stadsdeel']).agg({
-    'gem_intensiteit': 'sum',
-    'gem_snelheid': 'mean'
-}).reset_index()
-
-df_2019['start_meetperiode'] = pd.to_datetime(df_2019['start_meetperiode'])
-df_2019['1W_bin'] = df_2019['start_meetperiode'].dt.to_period('W').dt.start_time
-
-fig_2019 = px.bar(df_2019, 
-             x='1W_bin', 
-             y='gem_intensiteit',
-             title="Intensiteit verkeerstromen in 2019 (per week)", 
-             labels={'1W_bin': 'Tijdstip (per week)', 'gem_intensiteit': 'Aantal'}, 
-             color_discrete_sequence=['coral'])
-#fig_2019.show()
-
-#print(df_2024.head(500))
-#Bewerking voor join
-
-#Streamlit app 
-st.title("Verkeersintensiteit tijdens COVID")
-st.markdown("_visualisatie_")
-
-figures = {"2019":fig_2019, "2020":fig_2020, "2021":fig_2021, "2022":fig_2022, "2023":fig_2023, "2024":fig_2024}
-option = st.selectbox("Selecteer een figuur om te weergeven", list(figures.keys()))
-st.plotly_chart(figures[option])
-
+###################################
+# CONCLUSIE? ######################
+###################################

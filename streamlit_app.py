f6189a5c95c65708102b42185fbe141023b46011
leetjes123 <lvdleetjes@gmail.com>
2024-09-26 08:40:02 +0200

    joe

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,27 +22,27 @@ df_covid = pd.DataFrame(data) #DEZE VEREIST NOG AANPASSING
 # VERKEERSINTENSITEIT DATA LADEN EN FEATURES TOEVOEGEN #
 ########################################################
 
-def load_and_cache_data():
+@st.cache_data
+def load_daily_data():
 
-    df_2019 = pd.read_csv('intensiteit2019.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie'])
-    df_2020 = pd.read_csv('intensiteit2020.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie'])
-    df_2021 = pd.read_csv('intensiteit2021.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie'])
-    df_2022 = pd.read_csv('intensiteit2022.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie'])
-    df_2023 = pd.read_csv('intensiteit2023.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie'])
-    df_2024 = pd.read_csv('intensiteit2024.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie'])
-
-    df = pd.concat([df_2019,df_2020,df_2021,df_2022,df_2023,df_2024], ignore_index=True)
-
-    df['start_meetperiode'] = pd.to_datetime(df['start_meetperiode'])
-    df['jaar'] = df['start_meetperiode'].dt.year
-    df['dag'] = df['start_meetperiode'].dt.day_name()
-    df['tijd'] = df['start_meetperiode'].dt.strftime('%H:%M')
-    df['1W_bin'] = df['start_meetperiode'].dt.to_period('W').dt.start_time
-    
+    df = pd.read_csv('intensiteit_daily_average.csv')
 
     return df
 
-df = load_and_cache_data()
+df_daily = load_daily_data()
+
+@st.cache_data
+def load_weekly_data(year):
+    df = pd.read_csv(f'intensiteit{year}_weekly.csv')
+    return df
+
+# Cache individual DataFrames
+df19 = load_weekly_data(2019)
+df20 = load_weekly_data(2020)
+df21 = load_weekly_data(2021)
+df22 = load_weekly_data(2022)
+df23 = load_weekly_data(2023)
+df24 = load_weekly_data(2024)
 
 ###################################
 # INTRODUCTIE EN COVID PLOT #######
@@ -53,21 +53,7 @@ df = load_and_cache_data()
 # PLOTS VAN THOMAS MET UITLEG #####
 ###################################
 
-@st.cache_data
-def load_data(year):
-    df = pd.read_csv(f'intensiteit{year}_weekly.csv')
-    return df
-
-# Cache individual DataFrames
-df19 = load_data(2019)
-df20 = load_data(2020)
-df21 = load_data(2021)
-df22 = load_data(2022)
-df23 = load_data(2023)
-df24 = load_data(2024)
-
-
-dict = {
+data = {
     2019 : df19,
     2020 : df20,
     2021 : df21,
@@ -77,10 +63,11 @@ dict = {
 }
 
 st.write('Uitleg')
-year = st.selectbox("Selecteer een jaar", [2019,2020,2021,2022,2023,2024])
-# yearlyData = df[df['jaar'] == year]
 
-weekFig = px.bar(dict[year], 
+year = st.selectbox("Selecteer een jaar", range(2019,2025))
+
+
+weekFig = px.bar(data[year], 
              x='week', 
              y='gem_intensiteit',
              title=f"Intensiteit verkeerstromen in {year} (per week)", 
@@ -100,21 +87,33 @@ st.plotly_chart(weekFig, use_container_width=True)
 
 #Dagelijkse data laden
 @st.cache_data
-def calculate_daily_average(df):
-    return df.groupby(['dag', 'tijd','jaar'])['gem_intensiteit'].mean().reset_index()
+def load_daily_data():
 
-df_daily = calculate_daily_average(df)
+    df = pd.read_csv('intensiteit_daily_average.csv')
+
+    return df
+
+df_daily = load_daily_data()
 
 #Dropdown box maken en filteren op dag
-st.write('Uitleg')
+st.write('''Wat was het effect van de COVID-19 pandemie op de verdeling van verkeersintensiteit binnen een dag?
+          Daarvoor kijken we naar onderstaande grafiek. Te zien is de berekende gemiddelde verkeersintensiteit per weekdag per jaar.
+         Wat op valt is dat de oude vetrouwde spitsuren niet zijn opgeschoven of uitgespreid, 
+         wat het geval zou zijn als er een stijging was in het aannemen van flexibele werktijden. 
+         Wel is te zien dat over het algemeen de verkeerintensiteit sterk daalde na de start van de COVID-19 pandemie. 
+         Kijkende naar de lijnen van 2023 en 2024, blijkt ook dat deze daling in verkeersintensiteit nog niet genihileerd is.
+         Mogelijk door het aanhouden van de thuiswerkcultuur.''')
+
 weekday = st.selectbox('Selecteer een dag', df_daily['dag'].unique())
+
 dailyData = df_daily[df_daily['dag'] == weekday]
 
 #Plot
 dayFig = px.line(dailyData, x='tijd', y='gem_intensiteit', color='jaar',
-              title=f'Traffic Intensity on {weekday} - Comparison Across Years',
-              labels={'tijd': 'Time', 'gem_intensiteit': 'Average Intensity', 'year': 'Year'})
+              title=f'Intensiteit verkeersstromen op {weekday} - Vergelijking 2019-2024',
+              labels={'tijd': 'Time', 'gem_intensiteit': 'Gemiddelde Intensiteit ()', 'year': 'Year'})
 
+dayFig.update_xaxes(rangeslider_visible=True)
 dayFig.update_layout(xaxis_title='Time of Day', yaxis_title='Average Intensity', hovermode='x')
 
 st.plotly_chart(dayFig, use_container_width=True)

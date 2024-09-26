2ef2eacbea5989684fab7048dbb0c9462849aeb8
leetjes123 <lvdleetjes@gmail.com>
2024-09-26 14:45:18 +0200

    preprocessing gefixt

--- preprocessing_daily.py
+++ preprocessing_daily.py
@@ -1,28 +1,19 @@
 import pandas as pd
 
-def aggregate_and_save(year):
-    # Read the CSV file for the given year
-    df = pd.read_csv(f'intensiteit{year}.csv', usecols=['start_meetperiode', 'gem_intensiteit'])
-    
-    # Convert start_meetperiode to datetime
-    df['start_meetperiode'] = pd.to_datetime(df['start_meetperiode'])
-    
-    # Extract the week number
-    df['week'] = df['start_meetperiode'].dt.isocalendar().week
-    
-    # Group by week number and sum the gem_intensiteit
-    weekly_data = df.groupby('week')['gem_intensiteit'].sum().reset_index()
-    
-    # Sort by week number
-    weekly_data = weekly_data.sort_values('week')
-    
-    # Save to a new CSV file
-    output_file = f'intensiteit{year}_weekly.csv'
-    weekly_data.to_csv(output_file, index=False)
-    print(f"Saved aggregated data for {year} to {output_file}")
+df_2019 = pd.read_csv('intensiteit2019.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie'])
+df_2020 = pd.read_csv('intensiteit2020.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie'])
+df_2021 = pd.read_csv('intensiteit2021.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie'])
+df_2022 = pd.read_csv('intensiteit2022.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie'])
+df_2023 = pd.read_csv('intensiteit2023.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie'])
+df_2024 = pd.read_csv('intensiteit2024.csv', usecols=['start_meetperiode', 'gem_intensiteit', 'id_meetlocatie'])
 
-# Process data for each year from 2019 to 2024
-for year in range(2019, 2025):
-    aggregate_and_save(year)
+df = pd.concat([df_2019,df_2020,df_2021,df_2022,df_2023,df_2024], ignore_index=True)
 
-print("Data aggregation complete for all years.")
+df['start_meetperiode'] = pd.to_datetime(df['start_meetperiode'])
+df['jaar'] = df['start_meetperiode'].dt.year
+df['dag'] = df['start_meetperiode'].dt.day_name()
+df['tijd'] = df['start_meetperiode'].dt.strftime('%H:%M')
+
+df_grouped = df.groupby(['dag', 'tijd','jaar'])['gem_intensiteit'].mean().reset_index()
+
+df_grouped.to_csv('intensiteit_daily_average.csv', index=False)
